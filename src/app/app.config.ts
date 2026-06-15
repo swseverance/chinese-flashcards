6f396c5c936ad7221340b0d37d33e4fe79bbe881
Sam Severance <swseverance@gmail.com>
2026-06-15 14:54:07 -0400

    Add PWA support with service worker, manifest, and app icons
    Registers Angular service worker for offline caching, adds a web manifest
    with brand blue theme/background color, and includes a full icon set so
    the app is installable on mobile home screens.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
+import { EditOutline, LeftCircleTwoTone, LeftOutline } from '@ant-design/icons-angular/icons';
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { provideNzIcons } from 'ng-zorro-antd/icon';
-import { EditOutline, LeftCircleTwoTone, LeftOutline } from '@ant-design/icons-angular/icons';
 import { routes } from './app.routes';
 
 registerLocaleData(en);
@@ -15,5 +16,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideNzI18n(en_US),
     provideNzIcons([LeftOutline, LeftCircleTwoTone, EditOutline]),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
